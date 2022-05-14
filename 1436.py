ebcd9df7106fe2647ab2eede9b067506b83d468b
kim-do-hyeon <pental@kakao.com>
2022-05-15 01:52:45 +0900

    1436 - 영화감독 숍

---
+++ 1436.py
@@ -0,0 +1,12 @@
+# 1436 - 영화감독 숌
+
+N = int(input())
+string = 666
+count = 0
+while True :
+    if '666' in str(string) :
+        count += 1
+    if count == N :
+        print(string)
+        break
+    string += 1
