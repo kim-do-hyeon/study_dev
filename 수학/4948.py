06d151121167be64101eceda407cab36ce84c240
kim-do-hyeon <pental@kakao.com>
2022-06-02 23:45:49 +0900

    Moving folder

--- 수학/4948.py
+++ 수학/4948.py
@@ -22,27 +22,27 @@
 #         print(len(result))
 
 
-''' 시간초과 2 '''
-from math import sqrt
-while True :
-    n = int(input())
-    if n == 0 :
-        break
-    count = 0
+# ''' 시간초과 2 '''
+# from math import sqrt
+# while True :
+#     n = int(input())
+#     if n == 0 :
+#         break
+#     count = 0
 
-    for i in range(n + 1, 2 * n + 1) :
-        if  i == 1 :
-            continue
-        elif i == 2 :
-            count += 1
-            continue
-        else :
-            for j in range(2, int(sqrt(i) + 1)) :
-                if i % j == 0 :
-                    break
-            else :
-                count += 1
-    print(count)
+#     for i in range(n + 1, 2 * n + 1) :
+#         if  i == 1 :
+#             continue
+#         elif i == 2 :
+#             count += 1
+#             continue
+#         else :
+#             for j in range(2, int(sqrt(i) + 1)) :
+#                 if i % j == 0 :
+#                     break
+#             else :
+#                 count += 1
+#     print(count)
 
 # 10 ~ 20
 # 11 13 17 19
